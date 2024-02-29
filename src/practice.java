9441f49645a12a5f8275cfee122f226aa591a9fb
profaka <prof.aka777@gmail.com>
2024-02-29 21:00:42 +0600

    a + b

--- src/practice.java
+++ src/practice.java
@@ -1,5 +1,8 @@
 public class practice {
     public static void main(String[] args) {
         System.out.println("hello world from practice");
+        int a = 5;
+        int b = 1;
+        System.out.println(a+b);
     }
 }
