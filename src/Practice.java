84cbea2fdff194edf26bfb6b65358633eb11e5c9
profaka <prof.aka777@gmail.com>
2024-02-29 21:03:32 +0600

    a + b and Class name

--- src/Practice.java
+++ src/Practice.java
@@ -1,4 +1,4 @@
-public class practice {
+public class Practice {
     public static void main(String[] args) {
         System.out.println("hello world from practice");
         int a = 5;
--- src/Practice2.java
+++ src/Practice2.java
@@ -1,4 +1,4 @@
-public class practice2 {
+public class Practice2 {
     public static void main(String[] args) {
         System.out.println("hello world from practice2");
     }
